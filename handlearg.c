1740233a2659980b1fe7395589c8451c85e6fe7f
sethsollie1 <solomon.onuma.a@gmail.com>
2023-05-23 18:42:17 -0700

    Task 2 - Handle command lines with arguments

---
+++ handlearg.c
@@ -0,0 +1,40 @@
+#include "shell.h"
+
+/**
+ * tokenize_command - tokenizes the command into arguments.
+ * Description: This function tokenizes the command into individual
+ *	arguments seperated by whitespace characters (tab, space, newline)
+ *	it then uses strtok to split the command into tokens, then
+ *	stores themin the args array.
+ *
+ * @command: Command string that should be tokenized.
+ * @args: The array where command arguments will be stored.
+ * Return: The number of arguments.
+ */
+
+int tokenize_command(char *command, char **args)
+{
+	int arg_count = 0;
+	char *token;
+	char *command_cpy = strdup(command); /* copy of command */
+
+	if (command_cpy == NULL)
+	{
+		perror("Error (strdup\n");
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(command_cpy, " \t\n");
+
+	while (token != NULL)
+	{
+		/* Allocate memory for each argument */
+		args[arg_count++] = strdup(token);
+		token = strtok(NULL, " \t\n");
+	}
+	args[arg_count] = NULL;
+
+	free(command_cpy); /* Free copied cmd string */
+
+	return (arg_count);
+}
