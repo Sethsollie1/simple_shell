5f39411552d8e21a5fda17bc89cbd8ea1f0f72e8
sethsollie1 <solomon.onuma.a@gmail.com>
2023-05-23 18:37:16 -0700

    Header filr for simple shell

---
+++ shell.h
@@ -0,0 +1,21 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_COMMAND_LENGTH 1024
+#define PROMPT "$ "
+
+void execute_command(char *command, char **args, char **environ);
+int tokenize_command(char *command, char **args);
+
+/* Environment variable */
+char **environ;
+
+#endif
