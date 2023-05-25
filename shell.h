9627c542602dd27f71359049713c9695fa55582f
sethsollie1 <solomon.onuma.a@gmail.com>
2023-05-24 19:23:49 -0700

    fix shell.h

--- shell.h
+++ shell.h
@@ -8,13 +8,19 @@
 #include <stdbool.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <signal.h>
 
 #define MAX_COMMAND_LENGTH 1024
 #define PROMPT "$ "
 
 void execute_command(char *command, char **environ);
+void display_prompt(void);
+void read_execute_command(void);
 
 /* Environment variable */
-char **environ;
+extern char **environ;
 
 #endif
