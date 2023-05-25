46c596e5a9f3864980d34b4d6f289b693302c640
sethsollie1 <solomon.onuma.a@gmail.com>
2023-05-24 19:23:02 -0700

    fix non interactive mode

--- shell.c
+++ shell.c
@@ -1,24 +1,27 @@
 #include "shell.h"
 
 /**
- * main - main function of shell.
- * Return:  Always return 0.
+ * display_prompt - Display the shell prompt.
  */
 
-int main(void)
+void display_prompt(void)
+{
+	write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
+}
+
+/**
+ * read_execute_commands - Read and execute commands.
+ */
+
+void read_execute_commands(void)
 {
 	char command[MAX_COMMAND_LENGTH];
-	bool is_terminal = isatty(STDIN_FILENO);
 	ssize_t num_read;
 	char *cmd;
 
 	while (true)
 	{
-		if (is_terminal)
-		{
-			/* Display the prompt "$ " */
-			write(STDOUT_FILENO, "$ ", 2);
-		}
+		display_prompt();
 
 		num_read = read(STDIN_FILENO, command, sizeof(command));
 
@@ -29,13 +32,9 @@ int main(void)
 		}
 		else if (num_read == 0)
 		{
-			/* End of file (Ctrl+D) */
 			break;
 		}
-
-		/* Remove trailing newline character */
-		if (command[num_read - 1] == '\n')
-		command[num_read - 1] = '\0';
+		command[strcspn(command, "\n")] = '\0';
 
 		if (strlen(command) == 0 || strspn(command, " \t") == strlen(command))
 		{
@@ -45,6 +44,50 @@ int main(void)
 		cmd = strtok(command, " \t");
 
 		execute_command(cmd, environ);
+	}
+}
+
+/**
+ * main - main function of shell.
+ * @argc: The number of command line arguments.
+ * @argv: The array of command line arguments.
+ * Return:  Always return 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int file;
+	ssize_t num_read;
+	char *cmd;
+	char line[MAX_COMMAND_LENGTH];
+
+	if (argc > 1)
+	{
+		file = open(argv[1], O_RDONLY);
+		if (file == -1)
+		{
+			perror("Error (open)");
+			exit(EXIT_SUCCESS);
+		}
+
+		while ((num_read = read(file, line, sizeof(line))) > 0)
+		{
+			line[strcspn(line, "\n")] = '\0';
+
+			if (strlen(line) == 0 || strspn(line, " \t") == strlen(line))
+			{
+				continue;
+			}
+
+			cmd = strtok(line, " \t");
+
+			execute_command(cmd, environ);
+		}
+		close(file);
+	}
+	else
+	{
+		read_execute_commands();
 	}
 	return (0);
 }
